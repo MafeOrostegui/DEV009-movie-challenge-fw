7b4c698532c5cd64fa96d826d25463b3049c1973
Mariaorostegui <mfop001new@gmail.com>
2023-10-03 16:58:56 -0500

    Creation of landing page for mobile and desktop version, with some styles. Creation of the service folder.

--- nook/src/app/app.module.ts
+++ nook/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LandingPageDesktopComponent } from './components/landing-page-desktop/landing-page-desktop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginComponent,
+    SignUpComponent,
+    LandingPageDesktopComponent
   ],
   imports: [
     BrowserModule,
