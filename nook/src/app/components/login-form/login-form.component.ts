3eb50cd47dd6b42b3088f66acbb9a74d0d6c9ae3
Mariaorostegui <mfop001new@gmail.com>
2023-10-05 19:19:15 -0500

    Responsive form design and error handling for user experience

--- nook/src/app/components/login-form/login-form.component.ts
+++ nook/src/app/components/login-form/login-form.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent {
   loginForm = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
@@ -19,31 +20,32 @@ export class LoginFormComponent implements OnInit {
     private router: Router
   ) { }
 
-  ngOnInit(): void {
-    const requestToken = localStorage.getItem('requestToken');
-    if (requestToken) {
-      this.authService.createSession(requestToken)
-        .subscribe({
-          next: (response: any) => {
-            console.log('Session ID:', response.session_id);
-          },
-          error: (error) => {
-            console.error('Error:', error);
-          }
-        });
-    } else {
-      console.error('The requestToken was not found in the localStorage.');
-    }
+  isMobile = window.innerWidth < 611;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.isMobile = window.innerWidth < 611;
   }
+
+  hide = true;
 
   onSubmit() {
     if (this.loginForm.valid) {
-      const sessionId = this.authService.getSessionId();
-      if (sessionId) {
-        console.log('Logging in with Session ID:', sessionId);
-        this.router.navigate(['/home']);
+      const requestToken = localStorage.getItem('requestToken');
+      if (requestToken) {
+        this.authService.createSession(requestToken)
+          .subscribe({
+            next: (response: any) => {
+              console.log('Session ID:', response.session_id);
+              localStorage.setItem('session_id', response.session_id);
+              this.router.navigate(['/home']);
+            },
+            error: (error) => {
+              console.error('Error:', error);
+            }
+          });
       } else {
-        console.error('Session ID not found.');
+        console.error('The requestToken was not found in the localStorage.');
       }
     }
   }
--- nook/src/app/components/sign-up/sign-up.component.ts
+++ nook/src/app/components/sign-up/sign-up.component.ts
@@ -1,40 +1,62 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
+
 export class SignUpComponent {
+  isMobile = window.innerWidth < 611;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.isMobile = window.innerWidth < 611;
+  }
+
   signUpForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    number: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    confirmPassword: new FormControl('', Validators.required),
   });
+
 
   constructor(private authService: AuthService) { }
   requestToken: string | null = null;
 
   onSubmit() {
     if (this.signUpForm.valid) {
-      this.authService.getToken()
-        .subscribe({
-          next: (response: any) => {
-            const requestToken = response;
-            const redirectTo = `http://localhost:4200/login`
-            this.requestToken = requestToken;
-            localStorage.setItem('requestToken', requestToken);
-            window.location.href = `https://www.themoviedb.org/authenticate/${requestToken}?redirect_to=${redirectTo}`;
-          },
-          error: (error) => {
-            console.error('Error:', error);
-          }
-        });
+      const password = this.signUpForm.get('password')?.value;
+      const confirmPassword = this.signUpForm.get('confirmPassword')?.value;
+      if (password === confirmPassword) {
+        this.authService.getToken()
+          .subscribe({
+            next: (response: any) => {
+              const requestToken = response;
+              const redirectTo = `http://localhost:4200/login`
+              const userData = {
+                firstName: this.signUpForm.get('firstName')?.value,
+                lastName: this.signUpForm.get('lastName')?.value,
+                email: this.signUpForm.get('email')?.value,
+              };
+              this.requestToken = requestToken;
+              localStorage.setItem('requestToken', requestToken);
+              localStorage.setItem('user_data', JSON.stringify(userData));
+
+              window.location.href = `https://www.themoviedb.org/authenticate/${requestToken}?redirect_to=${redirectTo}`;
+            },
+            error: (error) => {
+              console.error('Error:', error);
+            }
+          });
+      } else {
+        alert('Passwords do not match.');
+      }
     }
   }
 }
