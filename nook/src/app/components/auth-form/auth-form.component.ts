fd58faf702b7303b7df2d56c5fe949e6788d045a
Mariaorostegui <mfop001new@gmail.com>
2023-10-11 19:40:12 -0500

    form component SCSS file removed

--- nook/src/app/components/auth-form/auth-form.component.ts
+++ nook/src/app/components/auth-form/auth-form.component.ts
@@ -31,7 +31,6 @@ type ActionType = keyof typeof actionType;
     MatIconModule,
   ],
   templateUrl: './auth-form.component.html',
-  styleUrls: ['./auth-form.component.scss'],
 })
 export class AuthFormComponent implements OnInit {
 
